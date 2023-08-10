2e818dd313bc4c015fc4f759334ff91799c9cf8d
latch <jst.latch@gmail.com>
2023-08-10 16:29:28 +0300

    feat: add username query function

---
+++ brain_games/scripts/cli.py
@@ -0,0 +1,10 @@
+import prompt
+
+def welcome_user():
+        print(f"Welcome to the Brain Games!")
+        user_name = prompt.string('May I have your name? ')
+        print(f"Hello, {user_name}!")
+
+if __name__  == '__main__':
+        main()
+
