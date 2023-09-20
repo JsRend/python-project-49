1107d041c0163a67fda76794c7f1c9f5cf8af5ec
latch <jst.latch@gmail.com>
2023-09-20 15:05:22 +0300

    feat: add prime_game

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,38 @@
+import random
+
+from brain_games.games.brain_engine import starting_game, greeting
+
+number_round = 3
+
+
+def rand_operand(lower_bound=2, upper_bound=100):
+    return random.randint(lower_bound, upper_bound)
+
+
+def is_prime(x):
+    for i in range(2, (x//2)+1):
+        if x % i == 0:
+            return 'no'
+    return 'yes'
+
+
+def prime_game():
+    user_name = greeting()
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+    for count in range(int(number_round)):
+
+        number = rand_operand()
+        correct_answer = is_prime(number)
+
+        question = number
+
+        if starting_game(question, correct_answer):
+            continue
+        else:
+            return 0
+    print(f"Congratulations, {user_name}!")
+
+
+if __name__ == '__main__':
+    prime_game()
